92ad6648b436b8333433156983c2b270a25d6b28
yujiashun <yujiashun@zhixuezhen.com>
2020-03-12 14:34:43 +0800

    yjs

--- src/pages/list.js
+++ src/pages/list.js
@@ -51,14 +51,15 @@ export default class List extends React.Component {
   panResponder = () => {
     this._panResponder = PanResponder.create({
       // 要求成为响应者：
-      // onStartShouldSetPanResponder: (evt, gestureState) => true,
-      // onStartShouldSetPanResponderCapture: (evt, gestureState) => true,
+      onStartShouldSetPanResponder: (evt, gestureState) => true,
+      onStartShouldSetPanResponderCapture: (evt, gestureState) => false,  // 禁用捕获机制
       onMoveShouldSetPanResponder: (evt, gestureState) => true,
-      onMoveShouldSetPanResponderCapture: (evt, gestureState) => true,
+      onMoveShouldSetPanResponderCapture: (evt, gestureState) => false,  // 禁用捕获机制
 
       onPanResponderGrant: (evt, gestureState) => {
-        console.log(gestureState)
-        this.startX = gestureState.x0;
+        // console.log(evt, gestureState)
+        this.startX = evt.nativeEvent.pageX;
+        console.log(this.startX)
         return true;
         // 开始手势操作。给用户一些视觉反馈，让他们知道发生了什么事情！
 
@@ -72,10 +73,11 @@ export default class List extends React.Component {
       },
       onPanResponderTerminationRequest: (evt, gestureState) => true,
       onPanResponderRelease: (evt, gestureState) => {
-        if (this.startX <= 30 && gestureState.moveX >= 40) {
-          alert('goBack');
+        console.log(evt.nativeEvent.pageX)
+        if (this.startX <= 30 && evt.nativeEvent.pageX >= 40) {
+          this.props.navigation.goBack();
         }
-        console.log(gestureState)
+        // console.log(evt, gestureState)
         return true;
 
         // 用户放开了所有的触摸点，且此时视图已经成为了响应者。
