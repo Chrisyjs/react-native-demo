d2638b913bb20c85ac808ae775fef4a8e50a0b82
yujiashun <yujiashun@zhixuezhen.com>
2020-04-01 16:53:03 +0800

    feat：日历列表

--- src/pages/stackOther/Calendar/index.js
+++ src/pages/stackOther/Calendar/index.js
@@ -3,80 +3,60 @@ import { Text, StyleSheet, View, ScrollView } from 'react-native'
 import {Calendar, CalendarList, Agenda} from 'react-native-calendars';
 
 export default class Index extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      markedDates: {}
+    }
+  }
   render() {
+    const { markedDates } = this.state;
     return (
-      <ScrollView>
-        <Calendar
-          // Collection of dates that have to be colored in a special way. Default = {}
-          markedDates={{
-            '2020-03-20': {textColor: 'rgba(255, 98, 20, 1)'},
-            '2020-03-22': {startingDay: true, color: 'rgba(255, 98, 20, 1)', selected: true},
-            '2020-03-23': {selected: true, color: 'rgba(255, 98, 20, 1)'},
-            '2020-03-24': {selected: true, endingDay: true, color: 'rgba(255, 98, 20, 1)', },
-            '2020-03-04': {disabled: true, startingDay: true, color: 'rgba(255, 98, 20, .1)', endingDay: true}
-          }}
-          // Date marking style [simple/period/multi-dot/custom]. Default = 'simple'
-          markingType={'period'}
-        />
-
-        <Calendar
-          // Collection of dates that have to be colored in a special way. Default = {}
-          markedDates={{
-            '2020-03-22': {startingDay: true, customStyles: {
-              container: {
-                backgroundColor: 'rgba(255, 98, 20, .1)',
-                borderRadius: 0
-              },
-              text: {
-                color: 'black',
-                fontWeight: 'bold'
-              }
+      <View>
+      {/* CalendarList */}
+      <CalendarList
+        pastScrollRange={1}
+        futureScrollRange={1}
+        // selected={'2020-04-04'}
+        markedDates={markedDates}
+        onDayPress={(day) => this.setState({
+          markedDates: {
+            [day.dateString]: {
+              selected: true
             }
-       },
-            '2020-03-23': {selected: true, customStyles: {
-              container: {
-                backgroundColor: 'rgba(255, 98, 20, .1)',
-                borderRadius: 0
-              },
-              text: {
-                color: 'black',
-                fontWeight: 'bold'
-              }
-            }
-       },
-            '2020-03-24': {selected: true, endingDay: false, customStyles: {
-              container: {
-                backgroundColor: 'rgba(255, 98, 20, .1)',
-                borderRadius: 0
-              },
-              text: {
-                color: 'black',
-                fontWeight: 'bold'
-              }
-            }
-       },
-          }}
-          // Date marking style [simple/period/multi-dot/custom]. Default = 'simple'
-          markingType={'custom'}
-        />
-
-        <Calendar
-          // Collection of dates that have to be colored in a special way. Default = {}
-          renderItem={(day, firstItemInDay) => {
-            return <View>234</View> 
-          }}
-          dayComponent={({date, state}) => {
-            return (
-              <View style={{backgroundColor: 'yellow'}}>
-                <Text style={{textAlign: 'center', color: state === 'disabled' ? 'gray' : 'black'}}>
-                  {date.day}
-                </Text>
-              </View>
-            )
-          }}
-          // Date marking style [simple/period/multi-dot/custom]. Default = 'simple'
-        />
-      </ScrollView>
+          }
+        })}
+        // dayComponent={({date, state}) => {
+        //   return (<Text>{date.day}</Text>)
+        // }}
+        onDayChange={(day) => console.log(day, 'onDayChange')}
+        theme={{
+          backgroundColor: '#ffffff',
+          calendarBackground: '#ffffff',
+          textSectionTitleColor: '#b6c1cd',
+          selectedDayBackgroundColor: 'orange',
+          selectedDayTextColor: '#FFF',
+          todayTextColor: '#00adf5',
+          dayTextColor: '#2d4150',
+          textDisabledColor: '#d9e1e8',
+          dotColor: '#00adf5',
+          selectedDotColor: '#ffffff',
+          arrowColor: 'orange',
+          disabledArrowColor: '#d9e1e8',
+          monthTextColor: 'blue',
+          indicatorColor: 'blue',
+          // textDayFontFamily: 'monospace',
+          // textMonthFontFamily: 'monospace',
+          // textDayHeaderFontFamily: 'monospace',
+          textDayFontWeight: '300',
+          textMonthFontWeight: 'bold',
+          textDayHeaderFontWeight: '300',
+          textDayFontSize: 16,
+          textMonthFontSize: 16,
+          textDayHeaderFontSize: 16
+        }}
+      ></CalendarList>
+    </View>
     )
   }
 }
